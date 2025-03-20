8c7198b1f14c54f6b8b1581923b5b5fc4624f3d1
CameloDev <freefireops@outlook.com>
2025-03-20 18:11:41 -0400

    New Feature Final 20/03 Adicionado banco de dados da VPS e um erro de autenticacao por parte do front/back

--- src/main/java/appetito/apicardapio/repository/ProdutoRepository.java
+++ src/main/java/appetito/apicardapio/repository/ProdutoRepository.java
@@ -4,9 +4,10 @@ package appetito.apicardapio.repository;
 import appetito.apicardapio.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    List<Produto> findByCardapio(Long id); // Para listar produtos de um cardápio específico
-   List<Produto> findAllByAtivoTrue();
+    List<Produto> findAllByAtivoTrue();
+    List<Produto> findAllByCardapio(Long id);
 }
