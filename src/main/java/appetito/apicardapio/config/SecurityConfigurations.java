1c66bf8e4047c840a91d9669b98b4758874d8bd2
CameloDev <freefireops@outlook.com>
2025-03-20 16:42:18 -0400

    New Feature Final 20/03 Adicionado banco de dados da VPS e um erro de autenticacao por parte do front/back

--- src/main/java/appetito/apicardapio/config/SecurityConfigurations.java
+++ src/main/java/appetito/apicardapio/config/SecurityConfigurations.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,7 +27,7 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
@@ -54,7 +53,7 @@ public class SecurityConfigurations {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
@@ -64,7 +63,6 @@ public class SecurityConfigurations {
                         .allowCredentials(true);
             }
         };
-        return webMvcConfigurer;
     }
 
 
