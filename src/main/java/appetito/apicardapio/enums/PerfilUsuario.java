cf34a5ab7868965dbbf8a761064b260111f09205
CameloDev <prozinffzin03051@gmail.com>
2025-03-20 21:57:27 -0400

    New Feature Final 20/03 Adicionado banco de dados da VPS e um erro de autenticacao por parte do front/back e vou dormir

--- src/main/java/appetito/apicardapio/enums/PerfilUsuario.java
+++ src/main/java/appetito/apicardapio/enums/PerfilUsuario.java
@@ -1,11 +1,15 @@
 package appetito.apicardapio.enums;
 
 public enum PerfilUsuario {
+    //Nos developers
     ADMINISTRADOR,
+    // funcoes padroes da empresa
     COZINHEIRO,
     ATENDENTE,
     GERENTE,
+    //cliente é o default
     CLIENTE,
+    // sao superusuarios
     PATROCINADOR,
     COLABORADOR
 }
