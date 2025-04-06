6ac59f2b057efc0f54ee33523e52624823a8386a
CameloDev <freefireops@outlook.com>
2025-04-06 18:42:00 -0400

    New Feature 05/04

--- src/main/java/appetito/apicardapio/controller/ClienteController.java
+++ src/main/java/appetito/apicardapio/controller/ClienteController.java
@@ -13,12 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
