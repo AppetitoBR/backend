f2747ec16c48146b0aed9eda48d764979f466d1a
CameloDev <freefireops@outlook.com>
2025-04-14 16:53:03 -0400

    New Feature 14/04 arrumando umas cagadas

--- src/main/java/appetito/apicardapio/repository/UsuarioEstabelecimentoRepository.java
+++ src/main/java/appetito/apicardapio/repository/UsuarioEstabelecimentoRepository.java
@@ -2,10 +2,8 @@ package appetito.apicardapio.repository;
 import appetito.apicardapio.entity.Estabelecimento;
 import appetito.apicardapio.entity.UsuarioDashboard;
 import appetito.apicardapio.entity.UsuarioEstabelecimento;
-import appetito.apicardapio.enums.PapelUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
