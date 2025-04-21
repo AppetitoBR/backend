35db5591603f395eba23f453d76d73a68c537f6f
CameloDev <freefireops@outlook.com>
2025-04-21 18:01:05 -0400

    New Feature 21/04

--- src/main/java/appetito/apicardapio/service/EmailService.java
+++ src/main/java/appetito/apicardapio/service/EmailService.java
@@ -1,35 +1,51 @@
 package appetito.apicardapio.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import appetito.apicardapio.config.EmailProviderConfig;
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+
+import java.util.Properties;
 
 @Service
 public class EmailService {
 
-    @Value("${spring.mail.username}")
-    private String from;
+    @Value("${api.mail.username}")
+    private String remetente;
 
-    private final JavaMailSender javaMailSender;
+    @Value("${api.mail.password}")
+    private String senha;
 
-    public EmailService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
-
-    public String enviarEmailTexto(String destinatario, String assunto, String mensagem) {
+    public String enviar(String destinatario, String assunto, String corpo) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(from);
-            message.setTo(destinatario);
+            String host = EmailProviderConfig.getSmtpHost(remetente);
+
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(remetente, senha);
+                }
+            });
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(remetente));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
             message.setSubject(assunto);
-            message.setText(mensagem);
-            javaMailSender.send(message);
+            message.setText(corpo);
+
+            Transport.send(message);
             return "Email enviado com sucesso!";
         } catch (Exception e) {
             e.printStackTrace();
-            return "Ocorreu um erro ao enviar email!";
+            return "Erro ao enviar email: " + e.getMessage();
         }
     }
 }
