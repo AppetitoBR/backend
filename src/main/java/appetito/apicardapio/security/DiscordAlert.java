2466b13f1ba0f67ebc8ddcf0bccfbe5e25a2099a
CameloDev <freefireops@outlook.com>
2025-05-21 08:02:01 -0400

    Added Java Docs in Security Services

--- src/main/java/appetito/apicardapio/security/DiscordAlert.java
+++ src/main/java/appetito/apicardapio/security/DiscordAlert.java
@@ -7,11 +7,33 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+/**
+ * Serviço responsável por enviar mensagens de alerta para um canal do Discord
+ * utilizando Webhooks.
+ *
+ * <p>
+ * Este serviço pode ser utilizado para notificar eventos importantes do sistema,
+ * como falhas críticas, ações administrativas ou qualquer evento que demande atenção.
+ * </p>
+ */
 @Service
 public class DiscordAlert {
 
+    // URL do webhook do canal do Discord. Mantenha essa URL protegida em produção(Ex = colocar em um .env)
     private final String webhookUrl = "https://discord.com/api/webhooks/1357876872247120064/Fb4qOOlj5AVQa-AwZDb5whMog0EWrhl8PyFF76cE2Gqw-G3EX1hp_AevsFdEKOt2BgyL";
 
+    /**
+     * Envia uma mensagem de alerta para o canal do Discord configurado via Webhook.
+     *
+     * @param mensagem a mensagem a ser enviada. Caracteres especiais como aspas são escapados.
+     */
     public void AlertDiscord(String mensagem) {
         try {
             String json = String.format("{\"content\": \"%s\"}", mensagem.replace("\"", "\\\""));
@@ -30,3 +52,4 @@ public class DiscordAlert {
         }
     }
 }
+
