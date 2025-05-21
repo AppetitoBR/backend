9bc65468216b73afbd8cebc485ff0b969d6b4b27
CameloDev <freefireops@outlook.com>
2025-05-21 09:00:49 -0400

    Added Java Docs in Project

---
+++ src/test/java/appetito/apicardapio/ApiCardapioApplicationTests.java
@@ -0,0 +1,12 @@
+package appetito.apicardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiCardapioApplicationTests {
+    @Test
+    public void contextLoads() {
+
+    }
+}
