9eee4ce27e0cbd371369246f41b2cef644454ee5
CameloDev <freefireops@outlook.com>
2025-05-27 18:07:03 -0400

    Added Docs to Cardapio Service

--- src/main/java/appetito/apicardapio/controller/CardapioController.java
+++ src/main/java/appetito/apicardapio/controller/CardapioController.java
@@ -1,42 +1,26 @@
 package appetito.apicardapio.controller;
+
 import appetito.apicardapio.dto.cadastro.CardapioCadastro;
 import appetito.apicardapio.dto.detalhamento.CardapioDetalhamento;
 import appetito.apicardapio.entity.Cardapio;
-import appetito.apicardapio.entity.Estabelecimento;
 import appetito.apicardapio.entity.UsuarioDashboard;
-import appetito.apicardapio.entity.UsuarioEstabelecimento;
-import appetito.apicardapio.exception.ResourceNotFoundException;
-import appetito.apicardapio.repository.CardapioRepository;
-import appetito.apicardapio.repository.EstabelecimentoRepository;
-import appetito.apicardapio.repository.UsuarioEstabelecimentoRepository;
 import appetito.apicardapio.service.CardapioService;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import appetito.apicardapio.dto.GetAll.CardapioDados;
-
 import java.nio.file.AccessDeniedException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/cardapio")
 public class CardapioController {
     private final CardapioService cardapioService;
-    private final CardapioRepository cardapioRepository;
-    private final EstabelecimentoRepository estabelecimentoRepository;
 
-    public CardapioController(CardapioService cardapioService, CardapioRepository cardapioRepository, EstabelecimentoRepository estabelecimentoRepository) {
+    public CardapioController(CardapioService cardapioService) {
         this.cardapioService = cardapioService;
-        this.cardapioRepository = cardapioRepository;
-        this.estabelecimentoRepository = estabelecimentoRepository;
     }
 
     // DASHBOARD
